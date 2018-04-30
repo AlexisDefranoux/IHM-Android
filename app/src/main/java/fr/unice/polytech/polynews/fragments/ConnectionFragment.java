6e7a22e1831edc097d1b625b0716e7da457c4b5a
François Nespoulous <francois.nespoulous@etu.unice.fr>
2018-04-30 16:25:08 +0200

    Add a connection page

---
+++ app/src/main/java/fr/unice/polytech/polynews/fragments/ConnectionFragment.java
@@ -0,0 +1,59 @@
+package fr.unice.polytech.polynews.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import fr.unice.polytech.polynews.ConnectionActivity;
+import fr.unice.polytech.polynews.MainActivity;
+import fr.unice.polytech.polynews.R;
+
+/**
+ * Created by Alex on 25/03/2018.
+ */
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ConnectionFragment extends Fragment implements View.OnClickListener {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private View rootView;
+    private Button btnClickMe;
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public ConnectionFragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static ConnectionFragment newInstance() {
+        ConnectionFragment fragment = new ConnectionFragment();
+        Bundle args = new Bundle();
+        Log.i("PlaceHolder", "message");
+        args.putInt(ARG_SECTION_NUMBER, 1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.connection, container, false);
+        btnClickMe = (Button) rootView.findViewById(R.id.button);
+        btnClickMe.setOnClickListener(ConnectionFragment.this);
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this.getContext(), ConnectionActivity.class));
+    }
+}

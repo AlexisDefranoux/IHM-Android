27114609c457f14a3a751293e2bc08df67f478ab
François Nespoulous <francois.nespoulous@etu.unice.fr>
2018-05-13 20:12:28 +0200

    Now finds location even if we don't opened maps before !

--- app/src/main/java/fr/unice/polytech/polynews/fragments/AddFragment.java
+++ app/src/main/java/fr/unice/polytech/polynews/fragments/AddFragment.java
@@ -1,9 +1,9 @@
 package fr.unice.polytech.polynews.fragments;
 
-import android.content.Context;
+import android.Manifest;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -15,34 +15,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.concurrent.TimeUnit;
+
 import fr.unice.polytech.polynews.R;
 
 import static com.google.android.gms.plus.PlusOneDummyView.TAG;
 
-/**
- * Created by Alex on 25/03/2018.
- */
-
-/**
- * A placeholder fragment containing a simple view.
- */
 public class AddFragment extends Fragment implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
     /**
      * The fragment argument representing the section number for this
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
-    private Button btnClickMe;
     private View rootView;
     private GoogleApiClient mGoogleApiClient;
     private Location mLocation;
-    private LocationManager locationManager;
     private LocationRequest mLocationRequest;
     private double latitude;
     private double longitude;
@@ -79,7 +73,7 @@ public class AddFragment extends Fragment implements View.OnClickListener, Googl
         textSomethingElse.setText("SomethingElse : ");
         textAnotherThing.setText("AnotherThing : ");
 
-        btnClickMe = (Button) rootView.findViewById(R.id.buttonContinue);
+        Button btnClickMe = (Button) rootView.findViewById(R.id.buttonContinue);
         btnClickMe.setOnClickListener(AddFragment.this);
 
         mGoogleApiClient = new GoogleApiClient.Builder(getContext())
@@ -87,7 +81,10 @@ public class AddFragment extends Fragment implements View.OnClickListener, Googl
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
-        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
 
         return rootView;
     }
@@ -108,45 +105,31 @@ public class AddFragment extends Fragment implements View.OnClickListener, Googl
 
     @Override
     public void onConnected(Bundle bundle) {
-        TextView textLocation = (TextView) rootView.findViewById(R.id.location);
-        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            textLocation.setText("Can't find location");
-            return;
-        }
-        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if(mLocation == null){
-            //startLocationUpdates();
-            textLocation.setText("Can't find location");
-            return;
-        }
-        //if (mLocation != null) {
-        latitude = mLocation.getLatitude();
-        longitude = mLocation.getLongitude();
-        textLocation.setText("Location is latitude " + latitude + " and longitude " + longitude);
-        //}
-    }
-
-    /*protected void startLocationUpdates() {
-        // Create the location request
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        // Request location updates
+        TextView location = (TextView) rootView.findViewById(R.id.location);
+        location.setText("Can't find location");
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            return; //Not enough permissions
         }
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
-                mLocationRequest, this);
-        Log.d("reque", "--->>>>");
-    }*/
+        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLocation == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                    mLocationRequest, this);
+        }
+        for (int i = 0; i<10 && mLocation == null; ++i) {
+            mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            try {
+                TimeUnit.SECONDS.sleep((long) 0.5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (mLocation != null) {
+            latitude = mLocation.getLatitude();
+            longitude = mLocation.getLongitude();
+            location.setText("Your location is latitude " + latitude + " and longitude " + longitude);
+        }
+    }
 
     @Override
     public void onConnectionSuspended(int i) {
@@ -156,12 +139,21 @@ public class AddFragment extends Fragment implements View.OnClickListener, Googl
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(getActivity(), 9000);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onResume() {
+        super.onResume();
         mGoogleApiClient.connect();
     }
 
@@ -172,8 +164,18 @@ public class AddFragment extends Fragment implements View.OnClickListener, Googl
             mGoogleApiClient.disconnect();
         }
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, (com.google.android.gms.location.LocationListener) this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
     @Override
     public void onLocationChanged(Location location) {
-
+        mLocation = location;
     }
 }

3d5592767867e5705f8d8f64f2ae30b559b98ccc
Florent ROBERT <florent.robert@etu.unice.fr>
2018-05-24 15:37:54 +0200

    mailAgainv38.46

--- app/src/main/java/fr/unice/polytech/polynews/activities/DetailsActivity.java
+++ app/src/main/java/fr/unice/polytech/polynews/activities/DetailsActivity.java
@@ -100,7 +100,7 @@ public class DetailsActivity extends AppCompatActivity {
         mailButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent emailIntent = new Intent(Intent.ACTION_VIEW);
-                Uri data = Uri.parse("mailto:" + mishap.getEmail() + "?subject=" + "" + "&body=" + "");
+                Uri data = Uri.parse("mailto:" + mishap.getEmail() + "?subject=" + "[PolytechIncident]"+mishap.getTitleMishap() + "&body=" + "");
                 emailIntent.setData(data);
                 startActivity(emailIntent);
 
